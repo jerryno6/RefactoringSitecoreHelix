1a01804c90b19fe9ea86b30ed8f6475dbf2a85e4
Vu Le <vu.le@onsolve.com>
2020-02-10 09:58:04 +0700

    add files for events.tac.local

---
+++ events.tac.local.Tests/Test1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace events.tac.local.Tests
+{
+    public class Test1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            Assert.True(false);
+        }
+    }
+}
